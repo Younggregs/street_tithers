b198f586b46a162913e3f539a3798bf9b93baec2
Retzam Tarle <Younggregs>
2019-08-29 12:07:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  
+</head>
+
+<body>
+  <script
+    src="https://www.paypal.com/sdk/js?client-id=AacnqJaEJbGudsWPeRUtGZDPh-vxEnYb4r3nyjFzTgN_ehl_k4wvt_M9MkKWkdp3h0zVCs_jJwS0TbXx">
+  </script>
+</body>
