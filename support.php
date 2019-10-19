c2c5e82c6299eb77a0986905f50c02a4a595d908
Retzam Tarle <Younggregs>
2019-10-19 09:50:18 +0100

    push push

---
+++ support.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+body {
+  background-color: linen;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+    color: purple;
+}
+
+.center {
+  margin: auto;
+  width: 30%;
+  padding: 100px 0;
+}
+
+
+.about {
+  background-color: white;
+  margin: 10px;
+  padding: 5px;
+  border: 3px solid purple;
+}
+
+.support {
+  background-color: purple;
+  margin: 10px;
+  padding: 5px;
+  color: white;
+  padding: 10px;
+  border: 3px solid orange;
+}
+
+</style>
+</head>
+<body>
+    
+    <div class="center">
+    <h1>Street Tithers</h1>
+        <section class="support">
+            Contact us: street.tithers@gmail.com
+            We'll love to hear from you!
+        </section>
+    </div>
+
+    <section class="about">
+        <p>
+        Street Tithers is a non-governmental and non-profit and non-denominational humanitarian 
+        aid organization with emphasis on love for humanity and being your brother’s keeper, 
+        using our 10% of our income or less, as stated in the holy book Malachi 3:10-11. "Bring 
+        ye all the Tithes into the Storehouse, that there may be meat in mine house, and prove me 
+        now herewith, said the Lord of hosts, if I will not open you the windows of Heaven, and 
+        pour you out a blessing, that there shall not be room enough to receive it. And I will rebuke
+        the devourer for your sakes, and he shall not destroy the fruits of your ground, neither shall 
+        your vine cast her fruit before the time in the field, saith the Lord of hosts." The holy book 
+        also says in the book of 1 Corinthians 6:19 that “your body is the temple of the lord” Also 
+        Ephesians 3:17 says: “That I may make my home in you”. Thus, every human is the house (Temple) 
+        of the lord, and tithing into people's lives anywhere is not just beautiful a thing to do but 
+        also living the words. Instead of paying your Tithe in the Church, we introduce what we call 
+        'Street Tithing'; paying your Tithes on the street or part of your Tithes, towards the cause of 
+        the less- privileged in the society on the street; towards healing the world and alleviating 
+        poverty in communities around the world. We provide a platform for you to reach out to the poor 
+        and under privileged in the society through Tithing'.
+        </p>
+    </section>
+
+</body>
+</html>
